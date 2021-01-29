11d1ac2776f648b087ddce50524d21b816e654e7
Salvador Villalon <salvadorvillalon54@gmail.com>
2021-01-28 20:47:50 -0800

    created data for proejcts

--- components/reusable/DataList/DataList.js
+++ components/reusable/DataList/DataList.js
@@ -59,13 +59,41 @@ export default function DataList(props) {
 		if (data !== null && data !== undefined) {
 			return Object.entries(data).map((dataArray) => {
 				return dataArray[1].map((dataEntry) => {
-					const repo = Object.values(dataEntry)[0];
-					const createdAt = repo.created_at;
-					const year = createdAt.split('-')[0];
-					const name = repo.name;
-					const gitHublink = 'https:' + repo.git_url.slice(4);
-					const projectHomePageLink = repo.homepage;
+					console.table(dataEntry);
+					console.log(Object.values(dataEntry)[0].year);
+					console.log(dataEntry.year);
+					console.log(Object.keys(dataEntry));
+					// let {
+					// 	year,
+					// 	description,
+					// 	made_at,
+					// 	tech,
+					// 	title,
+					// 	github_repo_link,
+					// 	homepage_link
+					// } = dataEntry[0];
+					// let madeAt = made_at;
+					// console.log({
+					// 	year,
+					// 	description,
+					// 	made_at,
+					// 	tech,
+					// 	title,
+					// 	github_repo_link,
+					// 	homepage_link
+					// });
 
+					const year = Object.values(dataEntry)[0].year;
+					const description = Object.values(dataEntry)[0].description;
+					const madeAt = Object.values(dataEntry)[0].made_at;
+					const tech = Object.values(dataEntry)[0].tech;
+					const title = Object.values(dataEntry)[0].title;
+					const gitHublink = Object.values(dataEntry)[0]
+						.github_repo_link;
+					const projectHomePageLink = Object.values(dataEntry)[0]
+						.homepage_link;
+					console.log({ year });
+					console.log({ description });
 					return (
 						<React.Fragment>
 							<TableRow>
@@ -91,18 +119,14 @@ export default function DataList(props) {
 									className='projectTitle'
 									align='left'
 								>
-									{name}
+									{title}
 								</TableCell>
 
-								<TableCell align='right'>
-									Calexico, CA
-								</TableCell>
+								<TableCell align='left'>{madeAt}</TableCell>
 
-								<TableCell align='right'>
-									Next.js,Bootstrap
-								</TableCell>
+								<TableCell align='left'>{tech}</TableCell>
 
-								<TableCell align='right'>
+								<TableCell align='left'>
 									<div className='linksContainer'>
 										<Link href={gitHublink}>
 											<a>
@@ -112,7 +136,7 @@ export default function DataList(props) {
 
 										<Link
 											href={{
-												pathname: projectHomePageLink,
+												pathname: projectHomePageLink
 											}}
 										>
 											<a>
@@ -121,13 +145,6 @@ export default function DataList(props) {
 										</Link>
 									</div>
 								</TableCell>
-								{/* <TableCell align='right'>
-									<Link href='https://www.google.com/maps'>
-										<a>
-											<i className='fas fa-external-link-alt faIcon' />
-										</a>
-									</Link>
-								</TableCell> */}
 							</TableRow>
 
 							<TableRow>
@@ -146,30 +163,17 @@ export default function DataList(props) {
 												<div className='col-md-12 col-sm-12 dataDropdownContainer'>
 													<div className='dataContainer'>
 														<p className='text10 sourceSansText whiteText boldText'>
-															About Personal
-															Website Version 3
+															About {title}
 														</p>
 														<p className='text10 sourceSansText whiteText'>
-															This is a 24 hours
-															Hackathon in
-															Chicago, Illinois. I
-															became Lead
-															Front-End Developer
-															to create the
-															Front-End of the
-															This was my first
-															hackathon and we
-															received 2nd place
-															winner out of
+															{description}
 														</p>
 														<p className='text10 sourceSansText whiteText'>
 															<b>
 																Technologies
 																Used:
 															</b>{' '}
-															HTML, CSS,
-															Bootstrap, Git,
-															GitHub, Django
+															{tech}
 														</p>
 													</div>
 
